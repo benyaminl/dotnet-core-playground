5499a2a32f46796389e2faf562a3af72c43a40aa
Benyamin Limanto <blbenyamin9@gmail.com>
2022-03-02 17:21:25 +0700

    Fixing the LF and also creating DB

--- SQLDocker/sql-init.sh
+++ SQLDocker/sql-init.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
 # Wait to be sure that SQL Server came up
-sleep 120s
+sleep 60
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 12345\!@Local -d master -i create-database.sql
+c=$(/opt/mssql-tools/bin/sqlcmd -U sa -P 12345\!@Local -d master -h -1 -Q "set nocount on;SELECT count(name) FROM master.dbo.sysdatabases WHERE name = 'coba'" -W)
+if [ $c -eq 0 ]
+then
+    echo "Creating Database..."
+    /opt/mssql-tools/bin/sqlcmd -U sa -P 12345!@Local -d master -i create-database.sql
+    echo "Database and Table created"
+else 
+    echo "Database Exists... no need to recreate DB"
+fi 
--- sqlserver.Dockerfile
+++ sqlserver.Dockerfile
@@ -2,8 +2,11 @@
 FROM mcr.microsoft.com/mssql/server:2019-latest
 USER root
 RUN mkdir -p /usr/src/app/
+# Copy all Initialize Script
 COPY ./SQLDocker/sql-entrypoint.sh /usr/src/app/sql-entrypoint.sh
 COPY ./SQLDocker/sql-init.sh /usr/src/app/sql-init.sh
+COPY ./SQLDocker/create-database.sql /usr/src/app/create-database.sql
+# Change the permision
 RUN chmod +x /usr/src/app/sql-init.sh
 RUN chmod +x /usr/src/app/sql-entrypoint.sh
 USER mssql
